b07e510de7b86ffda8b07f51be2c8524f21a8fd3
sso06030 <sso06030@khu.ac.kr>
2021-03-27 21:11:22 +0900

    Search, Detail Container

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,6 +4,7 @@ import Home from '../Routes/Home/index'
 import TV from '../Routes/TV/index'
 import Search from '../Routes/Search/index'
 import Header from '../Components/Header'
+import Detail from "../Routes/Detail/index"
 
 export default () => (
     <Router>
@@ -12,6 +13,8 @@ export default () => (
             <Route path="/" exact component={Home} />
             <Route path="/tv" exact component={TV} />
             <Route path="/search" component={Search} />
+            <Route path="/movie/:id" component={Detail} />
+            <Route path="/show/:id" component={Detail} />
             <Redirect from="*" to="/" />
         </Switch>
     </Router>
