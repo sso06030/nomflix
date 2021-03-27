ec47f21880b153eccaf3c419936a9161f631b2d9
sso06030 <sso06030@khu.ac.kr>
2021-03-28 01:06:34 +0900

    Home, TV Presenter

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,49 @@
-export default () => "TV";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Loader from "../../Components/Loader";
+import Section from "../../Components/Section";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const TVPresenter = ({topRated,popular,airingToday, loading, error}) =>
+  loading ? (
+    <Loader/>
+  ) : (
+    <Container>
+      {topRated && topRated.length>0 &&(
+        <Section title="Top Rated Shows">
+          {topRated.map(show => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length>0 &&(
+        <Section title="Popular Shows">
+          {popular.map(show => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+      {airingToday && airingToday.length>0 &&(
+        <Section title="Airing Today">
+          {airingToday.map(show => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+    </Container>
+  )
+;
+
+TVPresenter.propTypes = {
+  topRated: PropTypes.array,
+  popular: PropTypes.array,
+  airingToday: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default TVPresenter;
