3f50441c87d15d1aaf64c0094e3ed03a12c296f6
sso06030 <sso06030@khu.ac.kr>
2021-03-30 22:00:01 +0900

    Error Message Component

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import Loader from "../../Components/Loader";
 import Section from "../../Components/Section";
+import Message from "../../Components/Message";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -45,6 +46,10 @@ const SearchPresenter = ({movieResults,tvResults,searchTerm,handleSubmit, loadin
                 <span key={show.id}>{show.name}</span>
               ))}
             </Section>
+          )}
+          {error&&<Message color="#e74c3c" text={error}/>}
+          {tvResults&&movieResults&&tvResults.length===0&&movieResults.length===0&&(
+            <Message color="#95a5a6" text="Nothing found"/>
           )}
         </>
       )}
